de5f2b82163b8d7c4bc9477d26eaba4b9df59c87
refact0r <34758569+refact0r@users.noreply.github.com>
2025-02-16 14:39:34 -0800

    update starting position

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,9 @@
 	onMount(() => {
 		const map = new mapboxgl.Map({
 			container: 'map', // container ID
-			style: 'mapbox://styles/mapbox/streets-v12', // style URL
-			center: [-74.5, 40], // starting position [lng, lat]
-			zoom: 9 // starting zoom
+			style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
+			center: [-122.205, 47.613], // starting position [lng, lat]
+			zoom: 16 // starting zoom
 		});
 	});
 </script>
