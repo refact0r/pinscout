32ffa091a5b72b74561599ca2910048ff1ee7b21
Raymon Zhang <raymonzhang20@gmail.com>
2025-02-16 14:45:15 -0800

    add user location

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,18 @@
 			center: [-122.205, 47.613], // starting position [lng, lat]
 			zoom: 16 // starting zoom
 		});
+
+		map.addControl(
+			new mapboxgl.GeolocateControl({
+				positionOptions: {
+					enableHighAccuracy: true
+				},
+				// When active the map will receive updates to the device's location as it changes.
+				trackUserLocation: true,
+				// Draw an arrow next to the location dot to indicate which direction the device is heading.
+				showUserHeading: true
+			})
+		);
 	});
 </script>
 
