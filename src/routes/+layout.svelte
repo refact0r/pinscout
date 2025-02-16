3f12e839054cf50a8224a86aa0c50487f271f3ea
refact0r <34758569+refact0r@users.noreply.github.com>
2025-02-16 15:40:34 -0800

    add navigation control, test pins

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,30 +7,54 @@
 	mapboxgl.accessToken =
 		'pk.eyJ1IjoicmVmYWN0MHIiLCJhIjoiY203ODZndDB3MHI4bTJrcHVpcDl0a2NjYiJ9.oFH9TjRqRRobGDri9dbmfA';
 
+	const pins = [
+		[-122.205, 47.613],
+		[-122.21, 47.61],
+		[-122.215, 47.615],
+		[-122.22, 47.62]
+	];
+
+	let mapPins = [];
+
+	function updateVisiblePins(map) {
+		const bounds = map.getBounds();
+
+		// remove all existing pins
+		mapPins.forEach((marker) => marker.remove());
+		mapPins = [];
+
+		// add only pins that are within the current bounds
+		pins.forEach((markerCoords) => {
+			if (bounds.contains(markerCoords)) {
+				const marker = new mapboxgl.Marker().setLngLat(markerCoords).addTo(map);
+				mapPins.push(marker);
+			}
+		});
+	}
+
 	onMount(async () => {
 		// default position in case geolocation fails
 		let userLocation = [-122.205, 47.613];
 
 		// get user location if possible
-		try {
-			const position = await new Promise((resolve, reject) => {
-				if (!navigator.geolocation) {
-					reject(new Error('Geolocation not supported'));
-				}
-
-				navigator.geolocation.getCurrentPosition(
-					(position) => resolve(position),
-					(error) => reject(error),
-					{
-						enableHighAccuracy: true
-					}
-				);
-			});
-			userLocation = [position.coords.longitude, position.coords.latitude];
-			console.log('Got position:', position);
-		} catch (error) {
-			console.warn('Failed to get location:', error);
-		}
+		// try {
+		// 	const position = await new Promise((resolve, reject) => {
+		// 		if (!navigator.geolocation) {
+		// 			reject(new Error('Geolocation not supported'));
+		// 		}
+		// 		navigator.geolocation.getCurrentPosition(
+		// 			(position) => resolve(position),
+		// 			(error) => reject(error),
+		// 			{
+		// 				enableHighAccuracy: true
+		// 			}
+		// 		);
+		// 	});
+		// 	userLocation = [position.coords.longitude, position.coords.latitude];
+		// 	console.log('Got position:', position);
+		// } catch (error) {
+		// 	console.warn('Failed to get location:', error);
+		// }
 
 		// initialize map
 		const map = new mapboxgl.Map({
@@ -48,11 +72,22 @@
 			trackUserLocation: true,
 			showUserHeading: true
 		});
-		map.addControl(geolocate);
+		map.addControl(geolocate, 'bottom-right');
+		// add navigation control
+		const nav = new mapboxgl.NavigationControl({
+			visualizePitch: true
+		});
+		map.addControl(nav, 'bottom-right');
 
 		// turn on geolocation when map is loaded
 		map.on('load', () => {
 			geolocate.trigger();
+			updateVisiblePins(map);
+		});
+
+		// update visible pins when map moves
+		map.on('moveend', () => {
+			updateVisiblePins(map);
 		});
 	});
 </script>
