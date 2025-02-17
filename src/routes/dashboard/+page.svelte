94e5809b05cfb9a8dcdb677994079399251f6103
shibest <eerkingtheawesome@gmail.com>
2025-02-17 15:42:03 -0800

    ranking to dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -240,13 +240,13 @@
 							↪ This Season: <div class="right">+{userState.user?.points}</div>
 						</h3>
 						<h3 class="taut">
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#1</div>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#{userState.user?.pp_ranking}</div>
 						</h3>
 						<h3>
 							Lifetime PinPoints Gained: <div class="right">{userState.user?.points}</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.pp_ranking}</div>
 						</h3>
 						<h3 class="taut">
 							↪ From Quests: <div class="right">+{userState.user?.points}</div>
@@ -278,7 +278,7 @@
 							Total Quests Completed: <div class="right">{userState.user?.quests_completed}</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.quests_ranking}</div>
 						</h3>
 						<h3>
 							Total Verified Locations Marked: <div class="right">
@@ -286,31 +286,31 @@
 							</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.locations_ranking}</div>
 						</h3>
 						<h3>
 							Total Reviews Left: <div class="right">{userState.user?.num_reviews}</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.reviews_ranking}</div>
 						</h3>
 						<h3 class="taut">
 							↪ This Season: <div class="right">+{userState.user?.num_reviews}</div>
 						</h3>
 						<h3 class="taut">
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#1</div>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#{userState.user?.reviews_ranking}</div>
 						</h3>
 						<h3>
 							Total Statuses Updated: <div class="right">{userState.user?.status_updates}</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.status_ranking}</div>
 						</h3>
 						<h3 class="taut">
 							↪ This Season: <div class="right">+{userState.user?.status_updates}</div>
 						</h3>
 						<h3 class="taut">
-							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#1</div>
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;↪ Ranking: <div class="right">#{userState.user?.status_ranking}</div>
 						</h3>
 						<!--<h3>Total Approvals Given: <div class="right">{userState.user?.points}</div></h3>
 						<h3>Total Disapprovals Given: <div class="right">{userState.user?.points}</div></h3>
@@ -320,7 +320,7 @@
 							Friends Invited: <div class="right">{userState.user?.friends_invited}</div>
 						</h3>
 						<h3 class="taut">
-							↪ Ranking: <div class="right">#1</div>
+							↪ Ranking: <div class="right">#{userState.user?.friends_ranking}</div>
 						</h3>
 					</div>
 				</div>
