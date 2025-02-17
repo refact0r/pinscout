e27696c4977e24e2c46022cf59adbbda623951c5
shibest <eerkingtheawesome@gmail.com>
2025-02-17 09:17:44 -0800

    dashboard glowup

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,5 +1,26 @@
 <script>
 	import { userState } from '$lib/state.svelte';
+	import { onMount } from 'svelte';
+
+	import {WarningCircle, Question} from 'phosphor-svelte';
+
+	let greeting;
+	
+	onMount(()=>{
+		var now = new Date();
+		const hours = now.getHours();
+		if (hours < 2) {
+			greeting = "night";
+		} else if (hours < 12) {
+			greeting= "morning";
+		} else if (hours < 19) {
+			greeting="afternoon";
+		} else if (hours < 22) {
+			greeting="evening";
+		} else {
+			greeting="night"
+		}
+	});
 </script>
 
 <svelte:head>
@@ -7,13 +28,66 @@
 </svelte:head>
 
 <div class="content">
+	<h1>Your Dashboard</h1>
+	<p class="greeting">Good {greeting}, {userState.user?.name}.</p>
+	<br>
 	<div class="dashboard">
-		<h1>Your Dashboard</h1>
 		<div class="stats">
-			<div class="greeting">
-				<p>Good _, {userState.user?.name}.</p>
+			<h2>Total PinPoints <Question /> : <div class="right">{userState.user?.points}</div></h2>
+			<div class="scrollbar">
+				<div class="substats">
+					<h3>Ranking: <div class="right">#1</div></h3>
+					<h3>PinPoints Gained:</h3>
+					<h3 class="taut">↪ Past Week: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Month: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Year: <div class="right">+{userState.user?.points}</div></h3>
+					<h3>Lifetime PinPoints Gained: <div class="right">{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ From Quests: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ From Location-Marking: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ From Reviews: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ From Approvals: <div class="right">+{userState.user?.points}</div></h3>
+				</div>
 			</div>
-			<div class="points">{userState.user?.points} PP</div>
+		</div>
+		<div class="stats">
+			<h2>Total Activity: <div class="right">{userState.user?.points}</div></h2>
+			<div class="scrollbar">
+				<div class="substats">
+					<h3>Ranking: <div class="right">#1</div></h3>
+					<h3>Total Quests Completed: <div class="right">{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Ranking: <div class="right">#1</div></h3>
+					<h3 class="taut">↪ Past Month: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Year: <div class="right">+{userState.user?.points}</div></h3>
+					<h3>Total Verified Locations Marked: <div class="right">{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Ranking: <div class="right">#1</div></h3>
+					<h3 class="taut">↪ Past Month: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Year: <div class="right">+{userState.user?.points}</div></h3>
+					<h3>Total Reviews Left: <div class="right">{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Ranking: <div class="right">#1</div></h3>
+					<h3 class="taut">↪ Past Month: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Year: <div class="right">+{userState.user?.points}</div></h3>
+					<h3>Total Approvals Given: <div class="right">{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Ranking: <div class="right">#1</div></h3>
+					<h3 class="taut">↪ Past Month: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="taut">↪ Past Year: <div class="right">+{userState.user?.points}</div></h3>
+				</div>
+			</div>
+		</div>
+		<div class="stats">
+			<h2>Quest Board <Question /> : <div class="right"></div></h2>
+			<div class="scrollbar">
+				<div class="substats">
+					<h3 class="quest">Weekly Quest 1: Weekly Upkeep<div class="right"><WarningCircle /></div></h3>
+					<h3 class="taut">Visit a pinned location and update its status.<div class="right"></div></h3>
+					<h3 class="taut">↪ PinPoint Reward: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="quest">Weekly Quest 2: Thought-Provoking<div class="right"><WarningCircle /></div></h3>
+					<h3 class="taut">Leave a review on a pinned location.<div class="right"></div></h3>
+					<h3 class="taut">↪ PinPoint Reward: <div class="right">+{userState.user?.points}</div></h3>
+					<h3 class="quest">Lifetime Quest 1: The More the Merrier<div class="right"><WarningCircle /></div></h3>
+					<h3 class="taut">Invite a friend with a referral code. <Question /> <div class="right"></div></h3>
+					<h3 class="taut">↪ PinPoint Reward: <div class="right">+{userState.user?.points}</div></h3>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
@@ -21,20 +95,68 @@
 <style>
 	.content {
         width: 100%;
+		text-align: center;
+	}
+	h1 {
+        margin: 2rem 0rem 0rem;
+        font-size: 4rem;
+    }
+	h2 {
+		font-size: 2rem;
+		align-items: center;
+	}
+	h3 {
+		font-weight: 400;
+	}
+	.quest {
+		font-weight: 700;
 	}
 	.dashboard {
-		background-color: rgb(123, 195, 222);
-		border-radius: 1vw;
-		padding: 2vh 2vw;
-	}
-	.stats {
+		padding: 0rem 2rem;
 		display: flex;
 		gap: 2%;
 	}
+	.stats {
+		background-color: rgb(123, 195, 222);
+		border-radius: 1vw;
+		padding: 2vh 2vw;
+		flex: 32%;
+		text-align: left;
+		height: 56vh;
+	}
+	.scrollbar{
+		margin-top: -1rem;
+		margin-right: -1.25rem;
+		height: 40vh;
+		overflow: auto;
+	}
+	.substats{
+		padding-right: .75rem;
+
+	}
+	.taut{
+		margin-top: -1rem;
+	}
+	.right {
+		float:right;
+	}
 	.greeting {
-		flex: 36%;
+		font-size: 1.5rem;
+		margin: 1rem 0rem;
 	}
-	.summary {
-		flex: 62%;
-	}
+	
+	::-webkit-scrollbar {
+        width: .5rem;
+    }
+    ::-webkit-scrollbar-track {
+        background: rgb(0,0,0,0);
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: black;
+        border-radius: 1vh;
+		transition: .2s;
+    }
+    ::-webkit-scrollbar-thumb:hover {
+        background-color: rgb(234, 72, 78);
+    }
 </style>
