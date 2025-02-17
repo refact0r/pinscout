12d5babbd5ed98f6dce9a854ae5121bef6c987f1
shibest <eerkingtheawesome@gmail.com>
2025-02-16 16:57:26 -0800

    stuff

---
+++ src/routes/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+    let x = true;
+</script>
+
+<div class="header">
+    <a href="/">pinscout</a>
+    <div class="right">
+        {#if x}
+            <a href="/dashboard">dashboard</a>
+        {:else}
+            <a href="/login">login</a>
+        {/if}
+        <a href="/settings">settings</a>
+        <a href="/about">about</a>
+    </div>
+</div>
+
+<style>
+    .right{
+        float:right;
+    }
+</style>
