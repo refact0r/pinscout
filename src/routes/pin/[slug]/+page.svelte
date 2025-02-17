fc958518e7fa4c44259857155da429742c1bc91c
Raymon Zhang <raymonzhang20@gmail.com>
2025-02-17 10:44:31 -0800

    track selected pin

--- src/routes/pin/[slug]/+page.svelte
+++ src/routes/pin/[slug]/+page.svelte
@@ -1,20 +1,26 @@
 <script>
+	import { selectedPin } from '$lib/state.svelte.js';
 	import { colorMap } from '$lib/utils';
+	import { X } from 'phosphor-svelte';
 
 	let { data } = $props();
 
 	let pin = $state(data.pins.find((pin) => String(pin.id) === data.id));
 	$effect(() => {
 		pin = data.pins.find((pin) => String(pin.id) === data.id);
+		selectedPin.pin = data.id;
 	});
 </script>
 
 <div class="page">
-	<div class="tags">
-		<span class="tag" style="--color: {colorMap[pin.type]}">{pin.type}</span>
-		{#if pin.name.length > 0}
-			<span class="tag" style="--color: {colorMap[pin.type]}">{pin.subtype}</span>
-		{/if}
+	<div class="top">
+		<div class="tags">
+			<span class="tag" style="--color: {colorMap[pin.type]}">{pin.type}</span>
+			{#if pin.name.length > 0}
+				<span class="tag" style="--color: {colorMap[pin.type]}">{pin.subtype}</span>
+			{/if}
+		</div>
+		<a href="/" onclick={() => (selectedPin.pin = null)}><X size="1.5rem" /></a>
 	</div>
 	<h1>
 		{#if pin.name.length > 0}
@@ -30,6 +36,12 @@
 		width: 100%;
 		height: 100%;
 		padding: 1rem;
+	}
+
+	.top {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
 	}
 
 	.tags {
