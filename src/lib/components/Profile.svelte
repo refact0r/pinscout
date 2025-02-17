d90feaffa256dfe059f2e6cb2ce3c94c0cbb9324
Raymon Zhang <raymonzhang20@gmail.com>
2025-02-17 08:38:04 -0800

    login in profile button

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,25 +1,37 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { userState } from '$lib/state.svelte';
-	import { UserCircle } from 'phosphor-svelte';
+	import { login, logout } from '$lib/supabaseClient';
+	import { SignOut, UserCircle } from 'phosphor-svelte';
 	export let logged_in;
 </script>
 
 <div class="profile box">
-	<a class="link" href="/dashboard">
-		<div>{logged_in ? userState.user?.points : 0} PP</div>
-		<div class="avatar">
-			{#if logged_in}
+	{#if logged_in}
+		<a class="link" href="/dashboard">
+			<div>{userState.user?.points} PP</div>
+			<div class="avatar">
 				<img src={userState.user?.avatar_url} alt="" />
-			{/if}
-			<UserCircle />
-		</div>
-	</a>
+				<UserCircle size="1.5rem" />
+			</div>
+		</a>
+		<button class="logout" title="Sign Out" onclick={logout}><SignOut size="1.5rem" /></button>
+	{:else}
+		<button class="link" onclick={login}>
+			<div>Login</div>
+			<div class="avatar">
+				<UserCircle size="1.5rem" />
+			</div>
+		</button>
+	{/if}
 </div>
 
 <style>
 	.profile {
 		width: fit-content;
+		display: flex;
+		align-items: center;
+		overflow: hidden;
 	}
 	.link {
 		padding: 0.75rem 1rem;
@@ -34,6 +46,7 @@
 	.link:hover {
 		color: rgb(234, 72, 78);
 		cursor: pointer;
+		background-color: var(--border);
 	}
 	a {
 		display: flex;
@@ -51,5 +64,29 @@
 		position: absolute;
 		top: 0;
 		left: 0;
+	}
+
+	.logout {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1.5rem;
+		padding: 0.75rem;
+		height: calc(1lh + 1.5rem);
+		position: relative;
+	}
+
+	.logout::before {
+		content: '';
+		width: 1px;
+		height: 1.5rem;
+		background-color: #999999;
+		position: absolute;
+		left: 0;
+		transform: translateX(-50%);
+	}
+
+	button:hover {
+		background-color: var(--border);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { userState } from '$lib/state.svelte';
 	import { logout } from '$lib/supabaseClient';
 
-    import {WarningCircle, ArrowSquareOut} from 'phosphor-svelte'
+	import { WarningCircle, ArrowSquareOut } from 'phosphor-svelte';
 </script>
 
 <svelte:head>
@@ -11,36 +11,41 @@
 
 <!--This first case of if statement is useless since container size will be 0 if user is logged in, user can logout through dashboard-->
 {#if userState.user}
-	<button on:click={logout}>Sign Out</button>
 	<h1>{userState.user?.email}</h1>
 {:else}
-    <div class="inline">
-        <a class="a"><WarningCircle /></a>
-        <a class="a">Not Logged In</a>
-    </div>
-	<p>Login to mark new locations, place reviews, earn pinpoints, and more!&nbsp;&nbsp;<a class="link" href="/login" title="Login">Login<ArrowSquareOut/></a></p>
+	<div class="inline">
+		<a class="a"><WarningCircle /></a>
+		<a class="a">Not Logged In</a>
+	</div>
+	<p>
+		Login to mark new locations, place reviews, earn pinpoints, and more!&nbsp;&nbsp;<a
+			class="link"
+			href="/login"
+			title="Login">Login<ArrowSquareOut /></a
+		>
+	</p>
 {/if}
 
 <style>
-    .inline {
-        display: flex;
-        gap: .25rem;
-        align-items: center;
+	.inline {
+		display: flex;
+		gap: 0.25rem;
+		align-items: center;
 		font-size: 1.2rem;
-        padding-top:.75rem;
-    }
-    .a{
-        align-items: center;
-        display:flex;
-    }
-    .link{
-        align-items: center;
-        color:black;
-        text-decoration: underline;
-        transition: .2s;
-    }
-    .link:hover{
-        color:rgb(234, 72, 78);
-        text-decoration: underline;
-    }
+		padding-top: 0.75rem;
+	}
+	.a {
+		align-items: center;
+		display: flex;
+	}
+	.link {
+		align-items: center;
+		color: black;
+		text-decoration: underline;
+		transition: 0.2s;
+	}
+	.link:hover {
+		color: rgb(234, 72, 78);
+		text-decoration: underline;
+	}
 </style>
